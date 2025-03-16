bc65f1d6bcb7aa51971a0167bc0a03e37685ab21
mikostrz <mikostrz@d17-429-pc-05.ipa.ii.agh.edu.pl>
2025-03-16 11:53:56 +0100

    First commit

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,7 +2,9 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
 
 import com.company.enroller.model.Participant;
@@ -20,6 +22,31 @@ public class ParticipantService {
 		String hql = "FROM Participant";
 		Query query = connector.getSession().createQuery(hql);
 		return query.list();
+	}
+
+	public Participant findByLogin(String login){
+		String hql = "FROM Participant p WHERE p.login = :id";
+		Query query = connector.getSession().createQuery(hql);
+		query.setParameter("id", login);
+		return (Participant) query.uniqueResult();
+	}
+
+	public void addParticipant(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		transaction.commit();
+	}
+
+	public void removeParticipant(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(participant);
+		transaction.commit();
+	}
+
+	public void updateParticipant(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().update(participant);
+		transaction.commit();
 	}
 
 }
